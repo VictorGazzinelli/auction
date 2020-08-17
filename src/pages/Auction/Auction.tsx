a4e588d447eeb4347b4d4718c1d6ea05c5ef2cc1
VictorGazzinelli <vpezzigc@gmail.com>
2020-08-16 21:57:14 -0300

    New Features

--- src/pages/Auction/Auction.tsx
+++ src/pages/Auction/Auction.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useContext, useEffect } from 'react'
 import styled from 'styled-components'
-import { Input, Button, Checkbox, Table } from 'antd';
+import { Input, Button, Checkbox, Table, Drawer, DatePicker } from 'antd';
 import { useHistory } from 'react-router-dom';
 import ClickableText from '../../components/ClickableText';
 import { GlobalContext, IGlobalContext } from '../../App';
@@ -10,6 +10,7 @@ import { useDoRequest } from '../../hooks/useDoRequest';
 import auctionRequest from '../../services/auction/auctionRequest';
 import notificationUtils from '../../utils/notificationUtils';
 import { ICreateAccountOutput } from '../../services/account/accountInterface';
+import { PlusOutlined } from '@ant-design/icons';
 
 export default function Auction(){
     const [InitialValue, setInitialValue] = useState<number>(0);
@@ -17,6 +18,8 @@ export default function Auction(){
     const [OpenedAt, setOpenedAt] = useState<string>("");
     const [ClosedAt, setClosedAt] = useState<string>("");
     const [IsItemUsed, setIsItemUsed] = useState<boolean>(false);
+    const [drawerCreateAuctionVisible, setDrawerCreateAuctionVisible] = useState<boolean>(false);
+    const [drawerEditAuctionVisible, setDrawerEditAuctionVisible] = useState<boolean>(false);
     const [ListValue, setListValue] = useState<Array<IAuctionDTO>>([]);
     const history = useHistory();
 
@@ -135,17 +138,19 @@ export default function Auction(){
         RequestUtils.setAuthToken('');
     }
 
-    const InputLabel = (label: string, setState: any) => (
+    const InputLabel = (label: string, setState: any, type: string) => (
         <div className="label-input-wrapper">
             <label>{label}:</label>
-            <Input onChange={e => setState(e.target.value)}/>
+            {type !== "dateTime" && <Input onChange={e => setState(e.target.value)} type={type}/>}
+            {type === "dateTime" && <DatePicker style ={{width: '50%'}}/>}
         </div>
     )
 
     return(
+        <>
         <Wrapper>
             <div className='card-wrapper'>
-                <div className="cadastro-wrapper">
+                {/* <div className="cadastro-wrapper">
 
                     {InputLabel("Valor inicial", setInitialValue)}
                     {InputLabel("Nome do leilao", setName)}
@@ -159,13 +164,41 @@ export default function Auction(){
                         Item é usado
                 </Checkbox>
 
-                <Button type="primary" onClick={addAuction}>Cadastrar</Button>
+                <Button type="primary" onClick={() => setDrawerCreateAuctionVisible(true)}>Cadastrar</Button> */}
+                <ClickableText onClick={() => onLogout()}>Sair</ClickableText> 
                 <div className="table-wrapper">
                     <Table style={{height: 550}} scroll={{ y: 500 }} pagination={false} dataSource={ListValue} columns={columns}/>
                 </div>
-                <ClickableText onClick={() => onLogout()}>Sair</ClickableText> 
+                <Button type="primary" onClick={() => setDrawerCreateAuctionVisible(true)}>
+                    <PlusOutlined /> Novo Leilão
+                </Button>
             </div>
         </Wrapper>
+        <Drawer
+            title="Novo Leilão"
+            placement="right"
+            closable={true}
+            width={400}
+            onClose={() =>  setDrawerCreateAuctionVisible(false)}
+            visible={drawerCreateAuctionVisible}
+            footer = { <Button type="primary" onClick={() => console.log('ok')}>Cadastrar</Button>}
+        >
+            <div className='card-wrapper'>    
+                    <div className="cadastro-wrapper">
+                        {InputLabel("Valor inicial", setInitialValue, "number")}
+                        {InputLabel("Nome do leilão", setName, "string")}
+                        {InputLabel("Data abertura", setOpenedAt, "dateTime")}
+                        {InputLabel("Data fechamento", setClosedAt, "dateTime")}
+                    </div>
+                </div>
+            <Checkbox 
+            style={{marginBottom: 10}}
+            onChange={e => setIsItemUsed(e.target.checked)}
+            >
+                Item é usado
+            </Checkbox>
+        </Drawer>
+        </>
     ) 
 }
 
@@ -193,8 +226,8 @@ const Wrapper = styled.div`
         }
 
         .label-input-wrapper{
-            margin-right:10px;
-            margin-bottom:10px;
+            margin-right:16px;
+            margin-bottom:32px;
             label{
                 font-weight:bold;
             }
